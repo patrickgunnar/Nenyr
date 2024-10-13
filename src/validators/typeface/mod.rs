374d6a432b68739488923a04b2155c4ce0be5cde
patrickgunnar <patrickgunnar.me@gmail.com>
2024-10-13 18:08:05 -0300

    feat: Add Validators for Typefaces and Variable Values
    - Implemented `NenyrTypefaceValidator` trait to validate typeface paths.
      - The validation process includes:
        - Checking for empty paths.
        - Resolving the parent directory of the context path.
        - Verifying the existence of the typeface file in the filesystem.
        - Validating the file extension against allowed font types (`woff`, `woff2`, `ttf`, `otf`, `eot`, `svg`).
    
    - Added tests to ensure functionality:
      - `all_typefaces_are_valid`: Confirms that all specified valid typefaces return true.
      - `all_typefaces_are_not_valid`: Ensures invalid typeface paths return false.
      - `test_empty_typeface_path`: Checks that an empty typeface path is handled correctly.
    
    - Introduced `NenyrVariableValueValidator` trait for validating variable values used in the Nenyr DSL.
      - The validation rules encompass:
        - Checking for invalid characters.
        - Detecting incomplete functions missing closing parentheses.
        - Identifying unbalanced quotes.
        - Matching and invalidating comments.
        - Flagging known invalid keywords (e.g., `undefined`, `invalid`, `NaN`).
        - Validating URL formats, specifically ensuring `url()` contains content.
        - Detecting improper property formats.
    
    - Future improvements planned for enhanced regex patterns and validation rules as the framework evolves.
    
    All validators have been thoroughly tested to confirm their reliability and correctness in different scenarios.
    
    - Next steps involve further development of the Nenyr parser to integrate these validators and enhance overall validation logic.

--- src/validators/typeface/mod.rs
+++ src/validators/typeface/mod.rs
@@ -1,1 +1,128 @@
-pub trait NenyrTypefaceValidator {}
+use std::path::Path;
+
+/// `NenyrTypefaceValidator` is a trait responsible for validating the provided path to a typeface file.
+///
+/// # Overview
+///
+/// This trait defines a method `is_valid_typeface` that checks if a given `typeface_path` is valid relatively to a given `context_path`.
+/// The validation follows these steps:
+/// 1. **Empty Path Check**: The method first verifies whether the provided `typeface_path` is empty. If it is, the validation fails and returns `false`.
+/// 2. **Context Path Transformation**: The `context_path` is converted into a `Path` instance using `Path::new()`.
+/// 3. **Parent Directory Resolution**: It attempts to resolve the parent directory of the provided `context_path`. If the parent directory is found, the method joins this directory with the `typeface_path` to form the relatively typeface file path.
+/// 4. **Existence Check**: The method then checks if the resolved `joined_path` exists on the filesystem. If it doesn't exist, the validation fails and returns `false`.
+/// 5. **Extension Validation**: If the file exists, the method extracts the file extension from the `typeface_path`. It checks whether the extension matches one of the valid font extensions: `woff`, `woff2`, `ttf`, `otf`, `eot`, or `svg`.
+///
+/// If all checks are successful, the method returns `true`, indicating that the typeface path is valid. Otherwise, it returns `false`.
+///
+/// # Method
+///
+/// - `is_valid_typeface(&self, typeface_path: &str, context_path: &str) -> bool`
+///
+/// This method returns `true` if the typeface path is valid within the given context, otherwise returns `false`.
+///
+/// # Valid File Extensions
+/// - `.woff`
+/// - `.woff2`
+/// - `.ttf`
+/// - `.otf`
+/// - `.eot`
+/// - `.svg`
+pub trait NenyrTypefaceValidator {
+    /// Validates whether the provided `typeface_path` is a valid font file within the given `context_path`.
+    ///
+    /// # Arguments
+    ///
+    /// - `typeface_path`: The relative path to the typeface file being validated.
+    /// - `context_path`: The base context path from which the typeface path is being resolved.
+    ///
+    /// # Returns
+    ///
+    /// A boolean value:
+    /// - `true` if the `typeface_path` exists, and its extension matches one of the valid typeface extensions.
+    /// - `false` if the path is invalid, non-existent, or has an unsupported file extension.
+    fn is_valid_typeface(&self, typeface_path: &str, context_path: &str) -> bool {
+        if !typeface_path.is_empty() {
+            let context_path = Path::new(context_path);
+
+            if let Some(parent) = context_path.parent() {
+                let joined_path = parent.join(typeface_path);
+
+                if joined_path.exists() {
+                    let split_typeface = typeface_path.split(".");
+
+                    if let Some(ext) = split_typeface.last() {
+                        return vec!["woff", "woff2", "ttf", "otf", "eot", "svg"].contains(&ext);
+                    }
+                }
+            }
+        }
+
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NenyrTypefaceValidator;
+
+    struct Typeface {}
+
+    impl Typeface {
+        pub fn new() -> Self {
+            Self {}
+        }
+    }
+
+    impl NenyrTypefaceValidator for Typeface {}
+
+    #[test]
+    fn all_typefaces_are_valid() {
+        let typeface = Typeface::new();
+        let context_path = "src/validators/typeface/typeface_context.nyr";
+        let typeface_paths = vec![
+            "../../../mocks/typefaces/rosemartin.regular.otf",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.eot",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.svg",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.ttf",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.woff",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.woff2",
+        ];
+
+        for typeface_path in typeface_paths {
+            assert!(typeface.is_valid_typeface(typeface_path, context_path));
+        }
+    }
+
+    #[test]
+    fn all_typefaces_are_not_valid() {
+        let typeface = Typeface::new();
+        let context_path = "src/validators/typeface/typeface_context.nyr";
+        let typeface_paths = vec![
+            "../../../mocks/typefaces/rosemartin.regular.ot2",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.fot",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.sbg",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.tf",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.wof",
+            "../../../mocks/typefaces/showa-source-curry.regular-webfont.wof2",
+            "../../mocks/typefaces/rosemartin.regular.otf",
+            "../mocks/typefaces/showa-source-curry.regular-webfont.eot",
+            "mocks/typefaces/showa-source-curry.regular-webfont.svg",
+            "../../../typefaces/showa-source-curry.regular-webfont.ttf",
+            "../../../mocks/showa-source-curry.regular-webfont.woff",
+            "../../../mocks/typefaces",
+            "",
+        ];
+
+        for typeface_path in typeface_paths {
+            assert!(!typeface.is_valid_typeface(typeface_path, context_path));
+        }
+    }
+
+    #[test]
+    fn test_empty_typeface_path() {
+        let typeface = Typeface::new();
+        let context_path = "src/validators/import/typeface_context.nyr";
+
+        assert!(!typeface.is_valid_typeface("", &context_path));
+    }
+}
--- src/validators/variable_value/mod.rs
+++ src/validators/variable_value/mod.rs
@@ -1,1 +1,160 @@
-pub trait NenyrVariableValueValidator {}
+use regex::Regex;
+
+/// Trait for validating variable values used in the Nenyr DSL.
+///
+/// This trait provides a default method `is_valid_variable_value` that checks if a given string
+/// (representing a variable value) adheres to a set of rules designed to ensure the validity of
+/// the value within the context of Nenyr. The method returns `true` if the value is valid and `false` otherwise.
+///
+/// # Validation Rules
+///
+/// The following criteria are used to validate the variable value:
+///
+/// - **Invalid Characters**: Ensures that the value does not contain characters like `{`, `}`, `@`, `$`, `!`, `;`, and `:`.
+/// - **Incomplete Functions**: Detects incomplete functions (e.g., `rgb(`, `rgba(`, `hsl(`, `calc(`, `url(`, etc.) that are missing a closing parenthesis.
+/// - **Unbalanced Quotes**: Identifies values with an odd number of single (`'`) or double (`"`) quotes, which indicates unbalanced quotes.
+/// - **Comments**: Matches and invalidates values containing comments (e.g., `/* comment */`).
+/// - **Invalid Keywords**: Flags values that are known to be invalid, such as `undefined`, `invalid`, or `NaN`.
+/// - **Invalid URL Format**: Invalidates `url()` if it does not contain any content inside the parentheses.
+/// - **Invalid Property Format**: Detects and invalidates property-like values such as `property: value` where both the property and value are just words without proper context.
+///
+/// # Future Improvements
+///
+/// - This validator will be enhanced to cover additional invalid patterns and edge cases
+///   as the framework evolves. Future versions will focus on improving the regex patterns
+///   and potentially introducing more comprehensive validation rules.
+pub trait NenyrVariableValueValidator {
+    /// Validates the provided variable value against defined invalid patterns.
+    ///
+    /// Returns `true` if the variable value is valid, and `false` otherwise.
+    ///
+    /// # Parameters
+    ///
+    /// - `variable_value`: A string slice representing the variable value to validate.
+    fn is_valid_variable_value(&self, variable_value: &str) -> bool {
+        // Regular expression to match invalid characters such as `{`, `}`, `@`, `$`, `!`, `;`, and `:`.
+        let invalid_char_regex = Regex::new(r"[{}@$!;:]").unwrap();
+
+        // Regular expression to match incomplete functions, such as `rgb(`, `rgba(`, `hsl(`, `calc(`, or `url(`
+        // that do not have closing parentheses.
+        let incomplete_function_regex =
+            Regex::new(r"(rgb|rgba|hsl|calc|url|linear-gradient)\([^)]*$").unwrap();
+
+        // Regular expression to match unbalanced quotes. This regex matches values with an odd number of quotes.
+        let unbalanced_quotes_regex = Regex::new(r#"(?:(?:[^"]*"){1,})|(?:[^']*'){1,}"#).unwrap();
+
+        // Regular expression to match comments (e.g., `/* comment */`).
+        let comments_regex = Regex::new(r"/\*.*\*/").unwrap();
+
+        // Regular expression to match known invalid keywords such as `undefined`, `invalid`, and `NaN`.
+        let invalid_keyword_regex = Regex::new(r"^(undefined|invalid|NaN)$").unwrap();
+
+        // Regular expression to match invalid URL format, specifically `url()` with no content inside.
+        let invalid_url_regex = Regex::new(r#"url\(\s*\)"#).unwrap();
+
+        // Regular expression to match invalid property format, such as `property: value` where both are words.
+        let invalid_property_regex = Regex::new(r"^\w+\s*:\s*\w+$").unwrap();
+
+        // TODO: Improve this group later.
+        //let number_without_unit_regex = Regex::new(r"^\d+$").unwrap();
+        //let invalid_number_unit_regex = Regex::new(r"^\d+\s+[a-zA-Z]+$").unwrap();
+        //let invalid_unit_regex = Regex::new(r"^\d+[a-zA-Z]+$").unwrap();
+        //let invalid_negative_value_regex = Regex::new(r"^-?\d+(\.\d+)?[a-zA-Z]+$").unwrap();
+
+        // Validate the value by ensuring it does not match any of the invalid patterns.
+        !invalid_char_regex.is_match(variable_value)
+            && !incomplete_function_regex.is_match(variable_value)
+            && !unbalanced_quotes_regex.is_match(variable_value)
+            && !comments_regex.is_match(variable_value)
+            && !invalid_keyword_regex.is_match(variable_value)
+            //&& !invalid_color_name_regex.is_match(variable_value)
+            && !invalid_url_regex.is_match(variable_value)
+            && !invalid_property_regex.is_match(variable_value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NenyrVariableValueValidator;
+
+    struct Variable {}
+
+    impl Variable {
+        pub fn new() -> Self {
+            Self {}
+        }
+    }
+
+    impl NenyrVariableValueValidator for Variable {}
+
+    #[test]
+    fn all_variables_are_valid() {
+        let variable = Variable::new();
+        let valid_values = vec![
+            "#000000",
+            "red",
+            "calc(2 + 2)",
+            "linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)",
+            "flex",
+            "100%",
+            "80vh",
+            "70vw",
+            "300px",
+            "150rem",
+            "1px solid rgba(245, 40, 145, 0.8)",
+            "rgb(245, 40, 145)",
+            "hsl(205 100% 50%)",
+        ];
+
+        for val in valid_values {
+            assert!(variable.is_valid_variable_value(val));
+        }
+    }
+
+    #[test]
+    fn all_variables_are_not_valid() {
+        let variable = Variable::new();
+        let invalid_values = vec![
+            "#000000;",
+            "red;",
+            "calc(2 : 2)",
+            "linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12",
+            "flex@",
+            "100%!",
+            "80v$h",
+            "7{0vw}",
+            "undefined",
+            "150:rem",
+            "1px solid rgba(245, 40, 145, 0.8",
+            "rgb(245, 40, 145",
+            "hsl(205 100% 50%",
+        ];
+
+        for val in invalid_values {
+            assert!(!variable.is_valid_variable_value(val));
+        }
+    }
+
+    #[test]
+    fn performance_test_large_variables_valid_vector() {
+        let variable = Variable::new();
+        let valid_variables: Vec<String> = (0..1000)
+            .map(|i| format!("1px solid rgba(245, 40, 145, 0.{})", i))
+            .collect();
+
+        for valid_variable in valid_variables {
+            assert!(variable.is_valid_variable_value(&valid_variable));
+        }
+    }
+
+    #[test]
+    fn performance_test_large_variables_not_valid_vector() {
+        let variable = Variable::new();
+        let valid_variables: Vec<String> =
+            (0..1000).map(|i| format!("hsl(205 100% {}%", i)).collect();
+
+        for valid_variable in valid_variables {
+            assert!(!variable.is_valid_variable_value(&valid_variable));
+        }
+    }
+}
